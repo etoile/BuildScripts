72f8297dcc531aff6c29de01cf9220ab2430a9f6
Quentin Mathé <quentin.mathe@gmail.com>
2014-02-28 12:29:53 +0000

    Switched build-etoile.sh to use Etoile Git repository based on Eric's changes.
    Also fixed libdispatch header search paths when the build is not a test build.
    
    
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/BuildScripts@10488 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- build-etoile.sh
+++ build-etoile.sh
@@ -7,19 +7,28 @@ J=
 
 # TODO: Support building tagged versions in Etoile SVN
 
-if [ "$ETOILE_VERSION" = "stable" ]; then
-	ETOILE_REP_PATH=stable
-elif [ "$ETOILE_VERSION" = "trunk" ]; then
-	ETOILE_REP_PATH=trunk/Etoile
-fi
+#if [ "$ETOILE_VERSION" = "stable" ]; then
+#	ETOILE_REP_PATH=stable
+#elif [ "$ETOILE_VERSION" = "trunk" ]; then
+#	ETOILE_REP_PATH=trunk/Etoile
+#fi
 
 if [ -n "$ETOILE_VERSION" ]; then
 
+	LIBDISPATCH_SOURCE_DIR=$BUILD_DIR/libdispatch-objc2
+
 	echo "Fetching libdispatch into $PWD"
-	${SVN_ACCESS}svn.gna.org/svn/etoile/trunk/Dependencies/libdispatch-objc2
+	if [ ! -d $LIBDISPATCH_SOURCE_DIR ]; then
+		git clone https://github.com/etoile/libdispatch-objc2.git $LIBDISPATCH_SOURCE_DIR
+	else
+		cd $LIBDISPATCH_SOURCE_DIR
+		git pull
+		cd ..	
+	fi
+	#${SVN_ACCESS}svn.gna.org/svn/etoile/trunk/Dependencies/libdispatch-objc2
 
 	echo "Building & Installing libdispatch"
-	cd libdispatch-objc2/libdispatch
+	cd $LIBDISPATCH_SOURCE_DIR/libdispatch
 	rm -rf Build && mkdir Build
 	cd Build
 	if [ "$TEST_BUILD" = "yes" ]; then
@@ -33,21 +42,38 @@ if [ -n "$ETOILE_VERSION" ]; then
 		# we create symbolic links just to support building Etoile immediately.
 		ln -sf $PWD/lib/* $GNUSTEP_LOCAL_ROOT/Library/Libraries 
 		ln -sf $PWD/include/* $GNUSTEP_LOCAL_ROOT/Library/Headers
-		# If libdispatch is installed in /usr/local, clang searches /usr/local/include/dispatch/ 
-		# and not just /usr/local/include (not sure why). For other install locations, 
-		# we have declare the header search path for CoreObject or projects that import
-		# CoreObject headers.
-		export CPATH="$GNUSTEP_LOCAL_ROOT/Library/Headers/dispatch:$CPATH"
+	else
+		# If libdispatch is installed in /usr/local, but /usr/local is not in the search paths, 
+		# we create symbolic links to expose libdispatch to the Etoile build.
+		ln -sf /usr/local/lib/libdispatch* $GNUSTEP_LOCAL_ROOT/Library/Libraries 
+		ln -sf /usr/local/include/dispatch $GNUSTEP_LOCAL_ROOT/Library/Headers/dispatch
+	fi
+	# If libdispatch is installed in /usr/local, clang searches /usr/local/include/dispatch/ and 
+	# not just /usr/local/include (not sure why). For other install locations, we have to declare
+	# the header search path for CoreObject or projects that import CoreObject headers.
+	export CPATH="$GNUSTEP_LOCAL_ROOT/Library/Headers/dispatch:$CPATH"
 	fi
 	cd ../../..
 
 	if [ $STATUS -ne 0 ]; then exit 2; fi
 
+	ETOILE_SOURCE_DIR=$BUILD_DIR/etoile-${ETOILE_VERSION}
+
 	echo "Fetching Etoile into $PWD"
-	${SVN_ACCESS}svn.gna.org/svn/etoile/${ETOILE_REP_PATH} etoile-${ETOILE_VERSION}
+	if [ ! -d $ETOILE_SOURCE_DIR ]; then
+		git clone https://github.com/etoile/Etoile.git $ETOILE_SOURCE_DIR
+	else
+		cd $ETOILE_SOURCE_DIR
+		git pull
+		cd ..
+	fi 	
+	#${SVN_ACCESS}svn.gna.org/svn/etoile/${ETOILE_REP_PATH} etoile-${ETOILE_VERSION}
+
+	echo "Fetching Etoile Repositories"
+	cd $ETOILE_SOURCE_DIR
+	./etoile-fetch.sh
 
 	echo "Building & Installing Etoile"
-	cd etoile-${ETOILE_VERSION}
 	($DUMP_ENV) && ($MAKE_CLEAN) && ($MAKE_BUILD) && ($MAKE_INSTALL)
 	exit $?
 
